4faeb5d519d4820275258970bf3243fe878d1fa9
anita-sharma <2016uce1171@mnit.ac.in>
2020-08-27 15:25:07 +0530

    Updates

---
+++ precache-manifest.884cccc1afe44cfed1f53f255d2135d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4e110995792435f04daa152584b544",
+    "url": "/React-todoApp/index.html"
+  },
+  {
+    "revision": "57835d6fa8915424dbec",
+    "url": "/React-todoApp/static/css/main.cde0ae9a.chunk.css"
+  },
+  {
+    "revision": "f3cdcaabcaac243879e7",
+    "url": "/React-todoApp/static/js/2.b529977d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todoApp/static/js/2.b529977d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57835d6fa8915424dbec",
+    "url": "/React-todoApp/static/js/main.127e2de2.chunk.js"
+  },
+  {
+    "revision": "571eee9c8de3d136dce5",
+    "url": "/React-todoApp/static/js/runtime-main.6206c4bf.js"
+  }
+]);
